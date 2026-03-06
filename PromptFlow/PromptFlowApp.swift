0764f305b3f23ea84aae0c9bf23893d2410bcc29
Kirill Vasilyev <heybazilier@gmail.com>
2026-03-06 14:01:31 +0400

    Initial Commit

---
+++ PromptFlow/PromptFlowApp.swift
@@ -0,0 +1,32 @@
+//
+//  PromptFlowApp.swift
+//  PromptFlow
+//
+//  Created by Cyril Vasilyev on 6.03.2026.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct PromptFlowApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
